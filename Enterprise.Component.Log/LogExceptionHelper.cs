47afa85900d6994b10fdc1dc1e66510d7826e5dc
Ben <benjamin.w1031@gmail.com>
2016-12-04 13:22:48 -0600

    Added log to DB component, need to implement the details later.

--- Enterprise.Component.Log/LogExceptionHelper.cs
+++ Enterprise.Component.Log/LogExceptionHelper.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Reflection;
 using Enterprise.Core.Interface.Log;
 using Enterprise.Core.Interface.Common;
